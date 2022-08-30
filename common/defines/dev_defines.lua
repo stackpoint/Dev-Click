a59d9ca145a9dfa947bad5cad6417c317a55a69a
LoStack <stackpoint@users.noreply.github.com>
2022-08-29 19:25:22 -1000

    Synchronize with Steam Release

--- common/defines/dev_defines.lua
+++
@@ -1,3 +0,0 @@
--- AI will not develop provinces that have more development than this or DEVELOPMENT_CAP_MULT*original development (whichever is bigger)
-NDefines.NAI.DEVELOPMENT_CAP_BASE = 100 -- Default 10
-NDefines.NAI.DEVELOPMENT_CAP_MULT = 20  -- Default 2
--- descriptor.mod
+++ descriptor.mod
@@ -1,2 +1,9 @@
-name="dev-click wip"
-path="mod/Dev-Click"
+version="1.2"
+tags={
+	"Fixes"
+	"Gameplay"
+}
+name="Auto Dev"
+picture="thumbnail.png"
+supported_version="1.33.3"
+remote_file_id="2817151719"
